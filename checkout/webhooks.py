8e8ada3815aaa3bf51ddb065b0d73fdbe23773db
Joseph Ortiz <Se_sema_95@hotmail.com>
2024-12-16 18:05:55 +0000

    Add checkout form data cacheing in payment intent

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -24,7 +24,7 @@ def webhook(request):
 
   try:
     event = stripe.Webhook.construct_event(
-      payload, sig_header, wh_secret
+    payload, sig_header, wh_secret
     )
   except ValueError as e:
     # Invalid payload
@@ -56,20 +56,3 @@ def webhook(request):
   # Call the event handler with the event
   response = event_handler(event)
   return response
-
-  # print('Success!')
-  # return HttpResponse(status=200)
-
-  # # Handle the event
-  # if event.type == 'payment_intent.succeeded':
-  #   payment_intent = event.data.object # contains a stripe.PaymentIntent
-  #   print('PaymentIntent was successful!')
-  # elif event.type == 'payment_method.attached':
-  #   payment_method = event.data.object # contains a stripe.PaymentMethod
-  #   print('PaymentMethod was attached to a Customer!')
-  # # ... handle other event types
-  # else:
-  #   print('Unhandled event type {}'.format(event.type))
-  #   return HttpResponse(status=400)
-
-  # return HttpResponse(status=200)
